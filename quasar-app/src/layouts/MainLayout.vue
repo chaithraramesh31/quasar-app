deb7b1ee627b7a5ec7b3c598e28ef82015824676
Chaithra R <chaithraramesh31@gmail.com>
2023-08-22 01:05:42 +0530

    Added back functionality

--- quasar-app/src/layouts/MainLayout.vue
+++ quasar-app/src/layouts/MainLayout.vue
@@ -1,9 +1,9 @@
 <template>
   <q-layout view="hHh lpR fFf">
 
-    <q-header elevated class="bg-primary text-white">
+    <q-header elevated class="bg-primary text-white q-pt-sm q-pb-sm">
       <q-toolbar>
-        <q-toolbar-title>Notes App</q-toolbar-title>
+        <q-toolbar-title style="font-size: 40px;">Notes App</q-toolbar-title>
       </q-toolbar>
     </q-header>
 
--- quasar-app/src/pages/Note.vue
+++ quasar-app/src/pages/Note.vue
@@ -1,17 +1,18 @@
 <script>
 /* eslint-disable */
+import { computed, ref } from 'vue';
 import Container from 'src/components/Container.vue';
 import { useLocalNotes } from 'src/helper';
 import { useRoute, useRouter } from 'vue-router';
 
 export default {
     components: { Container },
-    name: 'NotePage',
+    name: 'PageNote',
     setup() {
         const notes = useLocalNotes();
         const router = useRouter();
         const route = useRoute();
-        const noteId = computed(() => parseInt(route.params.idx))
+        const noteId = computed(() => parseInt(route.params.id))
         const note = computed(() => notes.value[noteId.value])
 
         const remove = () => {
@@ -21,7 +22,7 @@ export default {
 
         const editing = ref(false)
 
-        return { note, remove, editing }
+        return { noteId, note, remove, editing }
     }
 }
 </script>
@@ -29,7 +30,33 @@ export default {
 <template>
     <q-page padding>
         <Container>
+            <div v-if="editing">
+                <form @submit="editing = false">
+                    <q-input filled label="Title" v-model="note.title"/>
+                <q-input class="q-mt-sm" filled label="Description" v-model="note.description" dense/>
+                <q-card flat bordered class="q-mt-sm">
+                    <q-editor v-model="note.content" min-height="5rem"/>
+                </q-card>
+                <div class="q-mt-md">
+                    <q-btn type="submit" class="q-ml-sm" color="positive">Done</q-btn>
+                </div>
+                </form>
+            </div>
 
+            <div v-else>
+                <div class="row items-center justify-between">
+                    <h3 class="q-mb-md q-mt-md">{{ note.title }}</h3>
+                    <div>
+                        <q-btn round color="secondary" icon="edit" @click="editing = true"/>
+                        <q-btn class="q-ml-sm" round color="red" icon="delete" @click="remove"/>
+                    </div>
+                </div>
+                <div>{{ note.description }}</div>
+                <div class="q-mt-md" v-html="note.content"/>
+                <div>
+                    <q-btn class="q-mt-md" color="primary" to="/">Back</q-btn>
+                </div>
+            </div>
         </Container>
     </q-page>
 </template>
