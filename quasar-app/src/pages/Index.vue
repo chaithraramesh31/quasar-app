4efd83581abf88d8fcb6aef1852f81ecf6b99c38
Chaithra R <chaithraramesh31@gmail.com>
2023-08-21 23:01:04 +0530

    Creating quasar app

---
+++ quasar-app/src/pages/Index.vue
@@ -0,0 +1,15 @@
+<script>
+/* eslint-disable */
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'PageIndex',
+    setup() {
+        
+    },
+})
+</script>
+
+<template>
+    <q-page></q-page>
+</template>
---
+++ quasar-blog/src/pages/IndexPage.vue
@@ -0,0 +1,38 @@
+<!-- <template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script> -->
+<template>
+  <div class="q-pa-md" style="max-width: 300px">
+    <q-input
+      v-model="text"
+      filled
+      type="textarea"
+    />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      text: ref('')
+    }
+  }
+}
+</script>
